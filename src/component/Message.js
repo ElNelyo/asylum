255e290c4296dcabdc47fac75de7d497a41261af
Unknown <charly.strn@gmail.com>
2019-09-11 15:05:09 +0200

    add css & images

--- src/component/Message.js
+++ src/component/Message.js
@@ -7,33 +7,71 @@ class Message extends React.Component {
   constructor(props) {
     super(props);
     this.messages = [];
-    
+    this.users = [];
+    this.current_user = "";
   
   }
 
   getMessages() {
       this.messages=  JSON.parse('[   {      "sender_id": "12313133",      "sender_name": "Maxime",      "receiver_id": "20113551",      "receiver_name": "Charly",      "message": "Salut, ceci est nouveau message de Maxime pour Charly",      "datetime": "2019-09-09:09:53",      "read": true   },   {      "sender_id": "01315886",      "sender_name": "Trinh",      "receiver_id": "20113551",      "receiver_name": "Charly",      "message": "Salut, ceci est nouveau message de Trinh pour Charly",      "datetime": "2019-09-09:09:53",      "read": false   },   {      "sender_id": "20113551",      "sender_name": "Charly",      "receiver_id": "01315886",      "receiver_name": "Trinh",      "message": "Salut, ceci est nouveau message de Charly pour Trinh",      "datetime": "2019-09-09:09:53",      "read": false   },   {      "sender_id": "20113551",      "sender_name": "Charly",      "receiver_id": "12313133",      "receiver_name": "Maxile",      "message": "Salut, ceci est nouveau message de Charly pour Maxime",      "datetime": "2019-09-09:09:53",      "read": true   }]');
   }
+  getUsers(){
+    this.users = 
+    [
+    
+      {
+        name: "Trinh",
+        image: require( '../img/avatar/trinh.png' )
+      },
+      {
+        name: "Maxime",
+        image: require( '../img/avatar/maxime.png' )
+      }
+    ]
+  }
+  onClickDiv = (column) => {
+   console.log(column);
+}
+
+
   render() {
     this.getMessages();
-    console.log(this.messages)
-    
-    return (
-      <div class="messages-container">
-      <ol class="messages">
+    this.getUsers();
+    return [
+  
+      <div>
+           <div class="contact-container">
+       
+            {this.users.map((value, index) => {
           
-            {this.messages.map((value, index) => {
-              if(value.sender_id=="20113551"){
-                return <li class="mine">{value.message}</li>
-              }else{
-                return <li >{value.message}</li>
-              }
-              
+        
+                return <img  onClick={() => this.onClickDiv(value.name)} class="avatar" src={value.image}></img>
+
+               
             })}
-         
-        </ol>
-       </div>
-    );
+        
+                  
+           </div>
+            <div class="messages-container">
+                  <ol class="messages">
+                        {this.messages.map((value, index) => {
+                          if(value.sender_id=="20113551"){
+                            return <li class="mine">{value.message}</li>
+                          }else{
+                            return <li >{value.message}</li>
+                          }
+                          
+                        })}
+                    
+                    </ol>
+                  </div>
+
+          </div>
+
+      
+
+     
+    ];
   }
 }
 
