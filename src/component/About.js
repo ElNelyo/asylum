49725f8f69e698afc9da5c319e6655c7b66d9d73
Nelyo <charly.strn@gmail.com>
2019-12-17 22:53:40 +0100

    embed iframe

--- src/component/About.js
+++ src/component/About.js
@@ -1,14 +1,39 @@
 import React from "react";
-
+import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed'
 class About extends React.Component {
+
+
+
+
+
   render() {
-    return React.createElement(
-      "div",
-      null,
-      "Salut ",
-      this.props.name
-    );
+    var myElement = <div style={{ width: "100%", height: 'auto' }}>
+    <ResponsiveEmbed aspectRatio="16by9">
+      <embed type="image/svg+xml" src="http://195.154.150.105/asylum/" />
+    </ResponsiveEmbed>
+  </div>
+    
+    
+//    <iframe src="http://195.154.150.105/asylum/" style={{ width: 100%, height: 'auto' }}></iframe>;
+       return [
+       
+        <div>
+
+        
+        
+        {myElement}
+
+      </div>
+
+
+
+
+    ];
+
   }
+
+
+
 }
 
 
